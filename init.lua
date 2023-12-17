8ec3304701f132a5073acca5b867e4ba5bb0b3c5
vf <vf_info@mail.ru>
2023-12-17 17:08:34 +0300

    🎉 initial commit

---
+++ init.lua
@@ -0,0 +1,79 @@
+return {
+  
+  plugins = {
+    "catppuccin/nvim",
+    {
+      -- https://github.com/rbong/vim-flog
+      "rbong/vim-flog",
+      lazy = true,
+      cmd = { "Flog", "Flogsplit", "Floggit" },
+      dependencies = {
+        "tpope/vim-fugitive",
+      },
+    },
+    {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+      end
+    },
+    {
+      "ggandor/leap.nvim",
+      keys = {
+        { "s", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+        { "S", "<Plug>(leap-backward-to)", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+        { "x", "<Plug>(leap-forward-till)", mode = { "x", "o" }, desc = "Leap forward till" },
+        { "X", "<Plug>(leap-backward-till)", mode = { "x", "o" }, desc = "Leap backward till" },
+        { "gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from window" },
+      },
+      opts = {},
+      init = function() -- https://github.com/ggandor/leap.nvim/issues/70#issuecomment-1521177534
+        vim.api.nvim_create_autocmd("User", {
+          callback = function()
+            vim.cmd.hi("Cursor", "blend=100")
+            vim.opt.guicursor:append { "a:Cursor/lCursor" }
+          end,
+          pattern = "LeapEnter",
+        })
+        vim.api.nvim_create_autocmd("User", {
+          callback = function()
+            vim.cmd.hi("Cursor", "blend=0")
+            vim.opt.guicursor:remove { "a:Cursor/lCursor" }
+          end,
+          pattern = "LeapLeave",
+        })
+      end,
+      dependencies = {
+        "tpope/vim-repeat",
+      },
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        opts.incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<CR>',
+            scope_incremental = '<CR>',
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>',
+          }
+        }
+      end,
+    }
+  }, 
+
+  polish = function()
+    local map = vim.api.nvim_set_keymap
+    -- Set key bindings
+    map("n", "<leader>gg", ":silent! Flog<CR>", { noremap = true, silent = true,  desc = "A-la Git Graph (vim-flog)" })
+  end,
+
+   -- Set colorscheme to use
+  colorscheme = "catppuccin-mocha",
+}
+
