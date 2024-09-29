5d3033b16a26faaa7e6db4b48312a6b2a0a70348
vf <vf_info@mail.ru>
2024-09-29 20:59:51 +0300

    🩹 enable neovide options

--- lua/polish.lua
+++ lua/polish.lua
@@ -12,7 +12,6 @@ events.subscribe({
   end,
 })
 
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 if vim.g.neovide == true then
   vim.g.neovide_cursor_animation_length = 0.05
@@ -22,6 +21,8 @@ if vim.g.neovide == true then
   vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
   vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
 end
+
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
